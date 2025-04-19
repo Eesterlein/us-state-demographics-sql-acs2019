15c378507301e1e33cec24c14b6148161d201e46
Elissa Esterlein <elissa.esterlein@gmail.com>
2025-04-18 19:11:00 -0600

    Update and rename sql_analysis to analysis.sql

--- analysis.sql
+++ analysis.sql
@@ -3,3 +3,13 @@ This project uses the **American Community Survey (ACS)** data from the U.S. Cen
 Skills used: Joins, CTE's, Case statments, Windows Functions, Aggregate Functions, Ordering and ranking, Data Formatting and Cleaning
 */
 
+##Poverty Rate + Median Income by State:
+SELECT
+ geo_id,
+ median_income,
+ poverty,
+ total_pop,
+ ROUND(poverty / total_pop, 4) AS poverty_rate
+FROM
+ `bigquery-public-data.census_bureau_acs.state_2019_5yr`
+ORDER BY poverty_rate DESC;
